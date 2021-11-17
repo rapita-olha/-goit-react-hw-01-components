95161a160fe8b0dfca329ed4b2cbc1854a02c668
Olha <gorokhovianka@gmail.com>
2021-11-17 18:18:48 +0200

    remove app from src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 // ----------------------------------------------------------------------
-import user from "../data/user.json";
-import statisticalData from "../data/statistical-data.json";
-import friendsData from "../data/friends.json";
-import transactions from "../data/transactions.json";
+import user from "./data/user.json";
+import statisticalData from "./data/statistical-data.json";
+import friendsData from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 const App = () => {
   return (
